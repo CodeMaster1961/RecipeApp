298d175e95f9898dedc7440851ca403487f402fa
aynac <omer.aynaci@hva.nl>
2024-11-13 15:13:40 +0100

    comments added

--- app/src/main/java/com/example/recipeapp/ui/RecipeInstructionsScreen.kt
+++ app/src/main/java/com/example/recipeapp/ui/RecipeInstructionsScreen.kt
@@ -11,11 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.recipeapp.R
 import com.example.recipeapp.ui.state.RecipeDetailsUiState
 
+/**
+ * The screen for displaying instructions of a recipe
+ * @author Ömer Aynaci
+ * @param recipeId the id of a specific recipe
+ * @param viewModel the view model of the recipe
+ * @param navigateBack action to navigate back to the previous screen
+ */
 @Composable
 fun RecipeInstructionsScreen(recipeId: Int, viewModel: RecipeViewModel, navigateBack: () -> Unit) {
     val recipeState = viewModel.recipeDetailsUiState
@@ -36,19 +46,32 @@ fun RecipeInstructionsScreen(recipeId: Int, viewModel: RecipeViewModel, navigate
     }
 }
 
-
+/**
+ * A section for instructions of a recipe
+ * @author Ömer Aynaci
+ * @param instructions a list of instructions of a recipe
+ * @param navigateBack button to navigate back to the previous screen
+ */
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun InstructionSection(instructions: List<String>, navigateBack: () -> Unit) {
     Scaffold(
         topBar = {
-            RecipeDetailTopAppBar(recipeTitle = "Instructions", navigateBack = navigateBack)
+            RecipeDetailTopAppBar(
+                recipeTitle = stringResource(id = R.string.instructions_title),
+                navigateBack = navigateBack
+            )
         }
     ) {
         InstructionsOverview(instructions = instructions)
     }
 }
 
+/**
+ * Displays the instructions of a recipe
+ * @author Ömer Aynaci
+ * @param instructions a list of instructions of a recipe
+ */
 @Composable
 fun InstructionsOverview(instructions: List<String>) {
     LazyColumn(
@@ -61,8 +84,8 @@ fun InstructionsOverview(instructions: List<String>) {
         instructions.forEachIndexed { index, instruction ->
             item {
                 Text(
-                    text = "${index + 1}. $instruction",
-                    fontSize = 19.sp,
+                    text = "${1 + index}. $instruction",
+                    fontSize = 18.sp,
                     textAlign = TextAlign.Start
                 )
             }
