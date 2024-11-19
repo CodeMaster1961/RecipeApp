e57a53b59609a3e6f92396bfe2b2b86f8d1f24fa
aynac <omer.aynaci@hva.nl>
2024-11-19 13:21:54 +0100

    comments added and unused imports removed

--- app/src/main/java/com/example/recipeapp/data/repository/RecipeImplementation.kt
+++ app/src/main/java/com/example/recipeapp/data/repository/RecipeImplementation.kt
@@ -1,25 +1,34 @@
 package com.example.recipeapp.data.repository
 
-import android.util.Log
 import com.example.recipeapp.data.dtos.Recipe
 import com.example.recipeapp.data.remote.RecipeAPI
-import retrofit2.Response
 import timber.log.Timber
 
 class RecipeImplementation(
     private val recipeAPI: RecipeAPI
 ) : RecipeRepository {
 
+    /**
+     * Fetches a list of recipes
+     * @author Ömer Aynaci
+     * @return a list of recipes
+     */
     override suspend fun getAllRecipes(): List<Recipe> {
         return try {
             val response = recipeAPI.getAllRecipes()
             response.body()?.recipes ?: emptyList()
         } catch (error: Exception) {
-            Log.d("RecipeImplementation", "Error: ${error.message}")
+            Timber.tag("RecipeImplementation").d("Error: %s", error.message)
             emptyList()
         }
     }
 
+    /**
+     * Gets a recipe by its id
+     * @author Ömer Aynaci
+     * @param id the id of the recipe
+     * @return an instance of recipe
+     */
     override suspend fun getRecipeById(id: Int): Recipe {
         return recipeAPI.getRecipeById(id)
     }
